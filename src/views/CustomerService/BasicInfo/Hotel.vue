0a884deb24af30655e11bc1687ab152024ea9b07
sean <sean24163@hotmail.com>
2020-05-12 17:42:18 +0800

    旅店資料

---
+++ src/views/CustomerService/BasicInfo/Hotel.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-card>
+      <div slot="header" class="clearfix">
+        <span>旅宿資料</span>
+        <el-button style="float: right; margin-right:10px" type="primary" plain @click="ShowOnMap()"
+          icon="el-icon-map-location"></el-button>
+      </div>
+      <el-form class="showInfo">
+        <el-form-item label="帳號">
+          <el-input v-model="hotelData.username" readonly></el-input>
+        </el-form-item>
+        <el-form-item label="旅宿名稱">
+          <el-input v-model="hotelData.name" readonly></el-input>
+        </el-form-item>
+        <el-form-item label="統編">
+          <el-input v-model="hotelData.businessNo" readonly></el-input>
+        </el-form-item>
+        <el-form-item label="證件號碼">
+          <el-input v-model="hotelData.identity" readonly></el-input>
+        </el-form-item>
+        <el-form-item label="旅宿地址">
+          <el-input v-model="hotelData.address" readonly></el-input>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HotelInfo",
+    props: {
+      hotelData: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {}
+    },
+    methods: {
+      ShowOnMap()
+      {
+        window.open("https://www.google.com/maps/search/?api=1&query=" + this.storeData.latitude + "," + this.storeData.longitude);
+      }
+    },
+  };
+
+</script>
